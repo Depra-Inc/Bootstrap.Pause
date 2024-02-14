dea46aa74ae70fdeb549c5a3fdc39dd62d90a2fe
godzzz <g0dzZz1lla@yandex.ru>
2024-02-15 00:57:04 +0400

    [v0.0.1] First release

---
+++ Runtime/PauseBootstrap.cs
@@ -0,0 +1,34 @@
+﻿// SPDX-License-Identifier: Apache-2.0
+// © 2024 Nikolay Melnikov <n.melnikov@depra.org>
+
+using System.Collections.Generic;
+using Depra.Bootstrap;
+using Depra.Inspector.SerializedReference;
+using Depra.IoC.QoL.Builder;
+using Depra.IoC.Scope;
+using Depra.Pause;
+using UnityEngine;
+
+namespace Client.Bootstrap
+{
+	[DisallowMultipleComponent]
+	public sealed class PauseBootstrap : MonoBehaviour, IBootstrapElement
+	{
+		[SubtypeDropdown] [SerializeReference] private IPauseListener[] _listeners;
+
+		void IBootstrapElement.PreInitialize(IContainerBuilder container)
+		{
+			var listeners = new List<IPauseListener>(_listeners);
+			listeners.AddRange(GetComponents<IPauseListener>());
+			container.RegisterSingleton(listeners.ToArray());
+			container.RegisterSingleton(GetComponent<IPauseInput>());
+			container.RegisterSingleton<IPauseService, PauseService>();
+		}
+
+		void IBootstrapElement.Initialize(IScope scope)
+		{
+			// TODO: Replace initialization with NonLazy.
+			scope.Resolve<IPauseService>();
+		}
+	}
+}
