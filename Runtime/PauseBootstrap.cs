18e39df326f2e679b8b5e67dd010cb57f2a42b31
godzzz <g0dzZz1lla@yandex.ru>
2024-02-23 03:14:07 +0400

    [v0.0.7] Refactoring

--- Runtime/PauseBootstrap.cs
+++ Runtime/PauseBootstrap.cs
@@ -2,41 +2,29 @@
 // © 2024 Nikolay Melnikov <n.melnikov@depra.org>
 
 using System.Collections.Generic;
-using Depra.Bootstrap;
 using Depra.Inspector.SerializedReference;
 using Depra.IoC.QoL.Builder;
 using Depra.IoC.Scope;
 using Depra.Pause;
 using UnityEngine;
 
-namespace Client.Bootstrap
+namespace Depra.Bootstrap.Pause
 {
 	[DisallowMultipleComponent]
 	public sealed class PauseBootstrap : MonoBehaviour, IBootstrapElement
 	{
-		[SerializeField] private ScenePauseInput[] _inputs;
 		[SubtypeDropdown] [SerializeReference] private IPauseListener[] _listeners;
 
 		void IBootstrapElement.InstallBindings(IContainerBuilder container)
 		{
-			var input = new ComposedPauseInput(_inputs);
-			container.RegisterSingleton<IPauseInput>(input);
-
+			var inputs = new List<IPauseInput>(GetComponents<IPauseInput>());
+			container.RegisterSingleton(inputs);
 			var listeners = new List<IPauseListener>(_listeners);
 			listeners.AddRange(GetComponents<IPauseListener>());
-			container.RegisterSingleton(listeners.ToArray());
-
+			container.RegisterSingleton(listeners);
 			container.RegisterSingleton<IPauseService, PauseService>();
 		}
 
-		void IBootstrapElement.Initialize(IScope scope)
-		{
-			// TODO: Replace initialization with NonLazy.
-			var service = scope.Resolve<IPauseService>();
-			foreach (var input in _inputs)
-			{
-				input.Initialize(service);
-			}
-		}
+		void IBootstrapElement.Initialize(IScope scope) { }
 	}
 }
---
+++ Runtime/PauseSceneBootstrap.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using Depra.IoC.Scope;
+using Depra.Pause;
+using UnityEngine;
+
+namespace Depra.Bootstrap.Pause
+{
+	public sealed class PauseSceneBootstrap : SceneBootstrap
+	{
+		[SerializeField] private List<ScenePauseInput> _inputs;
+
+		private IPauseService _service;
+
+		public override void Initialize(IScope scope)
+		{
+			_service = scope.Resolve<IPauseService>();
+			_service.AddRange(GetComponents<IPauseListener>());
+
+			foreach (var input in _inputs)
+			{
+				input.Initialize(_service);
+				_service.Add(input);
+			}
+		}
+
+		private void OnDestroy()
+		{
+			_service?.RemoveRange(_inputs);
+			_service?.RemoveRange(GetComponents<IPauseListener>());
+		}
+	}
+}
