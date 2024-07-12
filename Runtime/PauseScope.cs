fd1c207ad1bd6c21e803d9b4ce97d4b573e484f0
godzzz <g0dzZz1lla@yandex.ru>
2024-07-12 23:21:48 +0400

    [v0.1.6] Dependency updated

--- Runtime/PauseScope.cs
+++ Runtime/PauseScope.cs
@@ -1,18 +1,18 @@
 ﻿// SPDX-License-Identifier: Apache-2.0
 // © 2024 Nikolay Melnikov <n.melnikov@depra.org>
 
+using System;
 using System.Collections.Generic;
-using Depra.Bootstrap.Project;
+using Depra.IoC.Composition;
 using Depra.IoC.QoL.Builder;
 using Depra.Pause;
 using Depra.SerializeReference.Extensions;
-using UnityEngine;
-using static Depra.Bootstrap.Pause.Module;
 
 namespace Depra.Bootstrap.Pause
 {
-	[CreateAssetMenu(menuName = MENU_PATH + nameof(ProjectScope), fileName = nameof(PauseScope), order = DEFAULT_ORDER)]
-	public sealed class PauseScope : ProjectScope
+	[Serializable]
+	[SerializeReferenceMenuPath(nameof(PauseScope))]
+	public sealed class PauseScope : ILifetimeScope
 	{
 		[SerializeReferenceDropdown]
 		[UnityEngine.SerializeReference]
@@ -22,7 +22,7 @@ namespace Depra.Bootstrap.Pause
 		[UnityEngine.SerializeReference]
 		private List<IPauseListener> _listeners;
 
-		public override void Configure(IContainerBuilder builder) => builder
+		void ILifetimeScope.Configure(IContainerBuilder builder) => builder
 			.RegisterSingleton(_inputs)
 			.RegisterSingleton(_listeners)
 			.RegisterSingleton<IPauseService, PauseService>();
